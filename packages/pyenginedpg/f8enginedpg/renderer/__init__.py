43e3551382863fea70c5da58c5362bd209b85c4b
sis92 <sis92contact@gmail.com>
2026-01-02 18:02:26 -0500

    Remove deprecated operator instance, registry, and editor implementations
    - Deleted `instance.py`, `registry.py`, `operator_graph_editor.py`, and related renderer files to streamline the codebase.
    - Removed the `main.py` file and associated demo registry setup.
    - Cleaned up the `pyproject.toml` file as it is no longer needed.

--- packages/pyenginedpg/f8enginedpg/renderer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .renderers.base import BaseOpRenderer, NodeAttrUserData, LinkUserData
-from .registry import NodeRenderContext, RendererRegistry, BaseOpRenderer
-
-__all__ = ['NodeRenderContext', 'RendererRegistry', 'BaseOpRenderer', 'NodeAttrUserData', 'LinkUserData']
